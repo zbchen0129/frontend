c8faec7d71b6c1b66b56f6a10d37cade07f6f224
zbchen0129 <zbchen@uchicago.edu>
2023-06-20 01:32:56 -0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <input type="text" v-model="input" @input="onInput" placeholder="Type a state or territory">
+    <div v-for="suggestion in suggestions" :key="suggestion" @click="selectSuggestion(suggestion)">
+      {{ suggestion }}
+    </div>
+    <div id="map"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      input: '',
+      suggestions: [],
+      map: null,
+
+    };
+  },
+  mounted() {
+    window.initMap = this.initMap;
+  },
+  methods: {
+    onInput() {
+      axios.post('http://localhost:4000/graphql', {
+        query: `{ getSuggestions(input: "${this.input}") }`
+      }).then(response => {
+        this.suggestions = response.data.data.getSuggestions;
+      }).catch(error => {
+        console.log(error);
+      });
+    },
+    selectSuggestion(suggestion) {
+      this.input = suggestion;
+      this.suggestions = [];
+      this.highlightState(suggestion);
+    },
+    initMap() {
+      this.map = new window.google.maps.Map(document.getElementById('map'), {
+        center: { lat: 37.0902, lng: -95.7129 },
+        zoom: 5,
+      });
+    },
+    
+    highlightState(state) {
+      if (this.currentOverlay) {
+        this.currentOverlay.setMap(null);
+      }
+
+
+      fetch('us-states.json')
+        .then(response => response.json())
+        .then(data => {
+          
+          const stateFeature = data.features.find(feature => feature.properties.name === state);
+          if (stateFeature) {
+            this.currentOverlay = new window.google.maps.Data();
+            this.currentOverlay.addGeoJson(stateFeature);
+            this.currentOverlay.setStyle({
+              fillColor: 'red',
+              strokeWeight: 1
+            });
+            this.currentOverlay.setMap(this.map);
+            
+            
+            const bounds = new window.google.maps.LatLngBounds();
+            this.currentOverlay.forEach(feature => {
+              feature.getGeometry().forEachLatLng(latlng => {
+                bounds.extend(latlng);
+              });
+            });
+            this.map.fitBounds(bounds);
+          }
+        });
+    }
+  },
+};
 </script>
 
+
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#map {
+  height: 500px;
+  width: 100%;
 }
 </style>
